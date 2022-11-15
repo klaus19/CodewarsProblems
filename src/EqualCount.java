eaf22f1d7deea8dcabbf533534641ac98edfeda2
klaus19 <tejaskhartude@gmail.com>
2022-11-15 10:13:18 +0530

    Added problem of Xs and Os

---
+++ src/EqualCount.java
@@ -0,0 +1,27 @@
+public class EqualCount {
+
+    public boolean checkEqualCount(String str) {
+
+        int count_O = 0;
+        int count_X = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == 'o') {
+                count_O++;
+            } else if (str.charAt(i) == 'x') {
+                count_X++;
+            }
+        }
+        if (count_X == count_O)
+            return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        EqualCount eq = new EqualCount();
+        String s1 = "ooxx";
+        String s2 = "xooxx";
+        System.out.println(eq.checkEqualCount(s1));
+        System.out.println(eq.checkEqualCount(s2));
+    }
+}
